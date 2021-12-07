d8d0f1eb6c5cb97c777ed17d40ad8a4b97225daf
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-12-07 18:33:55 +0700

    feat(prepare): changelog.md is now gitignored and removed from gitignore on running pepare.sh

--- prepare.sh
+++ prepare.sh
@@ -9,12 +9,17 @@ if ! command -v mockery &>/dev/null; then
 	go install github.com/vektra/mockery/v2@latest
 fi
 
-if ! command -v git-chglog &> /dev/null; then
+if ! command -v git-chglog &>/dev/null; then
 	go install github.com/git-chglog/git-chglog/cmd/git-chglog@latest
 fi
 
-if ! command -v golangci-lint &> /dev/null; then
+if ! command -v golangci-lint &>/dev/null; then
 	go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.43.0
 fi
 
 lefthook install
+
+# remove CHANGELOG.md from gitignore
+sed -i '' -e 's#CHANGELOG.md##' .gitignore
+git add .gitignore
+LEFTHOOK=0 git commit -m "prepared repository"
